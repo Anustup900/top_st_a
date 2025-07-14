6b2bbd5b582c01ce9e8885e52a797af9db9b5906
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2025-07-14 16:51:56 +0530

    changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 
 def load_images(folder):
-    image_files = ['product.png', 'idea.png', 'base.png', 'tryon.png', 'tryon_fixed.png']
+    image_files = ['product.png', 'idea.png', 'base.png', 'tryon.png', 'tryon_fixed.png', 'tryon_fixed_prod.png']
     images = []
     for img in image_files:
         img_path = os.path.join(folder, img)
